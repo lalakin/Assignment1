4336c85936c32aee14006eb29ee59b9026e53504
lalakin <sofiamartin11@gmail.com>
2017-03-02 19:13:05 -0800

    8ii8i

---
+++ script.js
@@ -0,0 +1,161 @@
+/* STEP 1
+var someMonth;
+function theMonth 		// function to return current month
+currentMonth 		// a constant
+var summerMonth; 		// an array of summer months
+myLibraryaFunction 	// a function */
+
+/* STEP 2
+45 (numeric literal), "color"(string literal), true (Boolean literal), null (null literal) */
+
+/*STEP 3
+var x = 5 * (9 + 2);
+var sum = Math.PI + 76; */
+
+/*STEP 4
+var firstName;
+var lastName;
+var sAddress;
+var sCity;
+var sState;
+var nZip;
+var nCode;
+var nAge; 
+var referralSource;
+var mayWeContactYou; */
+
+/*STEP 5
+var firstName;
+firstName = "John";
+var lastName;
+lastName = "Smith";
+var sAddress;
+sAddress = "234 Main St.";
+var sCity;
+sCity = "Boston";
+var sState;
+sState = "MA";
+var nZip;
+nZip = 65478;
+var nCode;
+nCode = 128698;
+var nAge;
+nAge = 25;
+var referralSource;
+referralSource = "clinical psychologis";
+var mayWeContactYou;
+mayWeContactYou = true;
+
+var firstName = "John";
+var lastName = "Smith";
+var sAddress = "234 Main St.";
+var sCity = "Boston";
+var sState = "MA";
+var nZip = 65478;
+var nCode = 128698;
+var nAge = 25;
+var referralSource = "clinical psychologis";
+var mayWeContactYou = true;
+
+var firstName = "John", var lastName = "Smith", var sAddress = "234 Main St.", var sCity = "Boston", var sState = "MA", var nZip = 65478, var nCode = 128698, var nAge = 25, var referralSource = "clinical psychologis", var mayWeContactYou = true; */
+
+/*STEP 6
+var numStr = 2;
+var boolStr = true;
+var numBool = 17;
+window.console.log("He has been playing tennis for " + numStr + " years."); 
+window.console.log("Everything what was said is " + boolStr+ ".");
+window.console.log(true + numBool); */ 
+
+/*STEP 7
+var hobby = "watercolor";
+window.console.log("To improve his skill of " + hobby + " he took " + "some " + action + ".");
+var action = "lessons"; */
+
+/*STEP 8
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString); */
+
+/*STEP 9
+var x = 6;
+x = null;
+window.console.log(x);
+var y;
+window.console.log(y); */
+
+
+/*STEP 10
+window.console.log(typeof "Boston");
+window.console.log(typeof 5);
+window.console.log(typeof true);
+window.console.log(typeof new Date());
+window.console.log(typeof home); */
+
+/*STEP 11
+alert("Hello " + "Sofia Martin" + ", welcome to the JavaScript class!"); */
+
+/*STEP 12
+var name = "Sofia Martin";
+alert("Hello " + name + ", welcome to the JavaScript class"); */
+
+/*STEP 13
+var course = "JavaScript";
+var name = "Sofia Martin";
+alert("Hello " + name + ", welcome to the " + course + " class!"); */
+
+/*STEP 14
+var course = "JavaScript";
+var name = "Sofia Martin";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!"); */
+
+/*STEP 15
+var name = prompt("What is your name?");
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!"); */
+
+/*STEP 16
+var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!"); */
+
+/*STEP 17
+x = 10;
+y = 20;
+window.console.log(x+y); */
+
+/*STEP 18
+var x = 20;
+x += 20;
+window.console.log(x); */
+
+/*STEP 19
+var x = 20;
+x *= 5;
+window.console.log(x); */
+
+/*STEP 20
+var x = 20%3;
+x /= 1;
+window.console.log(x); */
+
+/*STEP 21
+var x = 7;
+var y = 11;
+window.console.log(x == 7 || y == 11); */
+
+/*STEP 22
+var x = 7;
+var y = 11;
+window.console.log(x > 7 && y < 11); */
+
+/*STEP 23
+var widget = new Object();
+window.console.log(typeof widget); */
+
+/*STEP 24
+var widget = new Object();
+window.console.log(widget instanceof Object); */
+
+/*STEP 25
+var widget = new Object();
+window.console.log(!(widget instanceof Object)); */
